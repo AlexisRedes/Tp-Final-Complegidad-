e0f64f9aba9db858feedf205eb849dc043a5e349
Alexis <alexisgamer100@gmail.com>
2020-11-28 14:33:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
